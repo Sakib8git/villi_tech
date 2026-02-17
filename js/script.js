e00b551317e647e961e70638a06e1b1e661f5fe2
Sakib8git <sakib30278@gmail.com>
2026-02-17 16:29:53 +0600

    hero section done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const menuToggle = document.getElementById("menu-toggle");
 const navList = document.getElementById("nav-list");
-
+// nav bar er responsive dropdown
 menuToggle.addEventListener("click", () => {
   navList.classList.toggle("active");
 });
