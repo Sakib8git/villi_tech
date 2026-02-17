188c46e7bbea1900e8458ff19ca96e0def759f14
Sakib8git <sakib30278@gmail.com>
2026-02-17 23:51:46 +0600

    bookNow btn eventlistener add

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const menuToggle = document.getElementById("menu-toggle");
 const navList = document.getElementById("nav-list");
-// nav bar er responsive dropdown
+const bookButtons = document.querySelectorAll(".book-now");
+// nav bar  dropdown
 menuToggle.addEventListener("click", () => {
   navList.classList.toggle("active");
+});
+// book now btn 
+bookButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("hi");
+  });
 });
